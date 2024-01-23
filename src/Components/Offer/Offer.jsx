51a73cbabca8a980327658d813529053e341e0af
Daniel Karanja <sirkaranja12@gmail.com>
2024-01-23 21:17:19 +0300

    scrollable arrows

--- src/Components/Offer/Offer.jsx
+++ src/Components/Offer/Offer.jsx
@@ -1,43 +1,63 @@
-import React from 'react'
-import './Offer.css'
-import expresso from '../../assets/expresso.png'
+import React, { useState } from 'react';
+import './Offer.css';
+
+import expresso1 from '../../assets/expresso.png';
+import expresso2 from '../../assets/expresso.png';
+import expresso3 from '../../assets/expresso.png';
 
 const Offer = () => {
+  const [currentCard, setCurrentCard] = useState(0);
+
+  const cardData = [
+    { image: expresso1, title: 'Expresso 1', price: 300 },
+    { image: expresso2, title: 'Expresso 2', price: 350 },
+    { image: expresso3, title: 'Expresso 3', price: 400 },
+    { image: expresso3, title: 'Expresso 3', price: 400 },
+    { image: expresso3, title: 'Expresso 3', price: 400 }
+  ];
+
+  const handleNext = () => {
+    setCurrentCard((prevCard) => (prevCard + 1) % cardData.length);
+  };
+
+  const handleBack = () => {
+    setCurrentCard((prevCard) => (prevCard - 1 + cardData.length) % cardData.length);
+  };
+
+  const renderCards = () => {
+    const startCardIndex = currentCard;
+    const endCardIndex = (currentCard + 2) % cardData.length;
+
+    return cardData.slice(startCardIndex, endCardIndex + 1).map((card, index) => (
+      <div key={index} className="card">
+        <img src={card.image} alt={card.title} />
+        <h4>{card.title}</h4>
+        <h5><span id='price'>Ksh</span>{card.price}</h5>
+      </div>
+    ));
+  };
+
   return (
     <>
-    <h5 id='titl'>SPECIAL OFFERS</h5>
-    <div className="offers-container">
+      <h5 id='titl'>SPECIAL OFFERS</h5>
+      <div className="offers-container">
         <div className="offer-title">
-            <h2>Popular customer<br/>
-                favorite</h2>
+          <h2>Popular customer<br/>
+            favorite</h2>
         </div>
-    <div className="card-container">
-        <div className="card">
-            <img src={expresso} alt="" />
-            <h4>Expresso</h4>
-            <h5><span id='price'>Ksh</span>300</h5>
-            
+        <div className="navigation-arrows">
+        <div className="arrow" onClick={handleBack}>{'<'}</div>
+        <div className="arrow" onClick={handleNext}>{'>'}</div>
+      </div>
+        <div className="card-container">
+          {renderCards()}
         </div>
-        <div className="card">
-            <img src={expresso} alt="" />
-            <h4>Expresso</h4>
-            <h5><span id='price'>Ksh</span>300</h5>
-            
-        </div>
-        <div className="card">
-            <img src={expresso} alt="" />
-            <h4>Expresso</h4>
-            <h5><span id='price'>Ksh</span>300</h5>
-            
-        </div>
+      </div>
 
-    </div>
-
-    </div>
+      {/* Navigation buttons */}
+     
     </>
-    
-    
-  )
-}
+  );
+};
 
-export default Offer
+export default Offer;
